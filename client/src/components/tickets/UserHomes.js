11c56f6578d5dbf4f55a56f5d2c265d3ba84dfb9
Jeffrey Weisel <jeffrey.weisel@kbwtech.com>
2024-02-05 10:45:31 -0600

    added headers to each view, changed card hover effect to just the image

--- client/src/components/tickets/UserHomes.js
+++ client/src/components/tickets/UserHomes.js
@@ -3,7 +3,13 @@ import { Button, Card, CardBody, CardImg, CardText } from "reactstrap";
 import { getHomes, listHome } from "../../DataManagers/homeManager";
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLocationDot,
+  faCalendarCheck,
+  faHouseUser,
+  faHouseCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Card.css";
 
 export default function UserHomes({ loggedInUser }) {
   const [homes, setHomes] = useState([]);
@@ -18,23 +24,40 @@ export default function UserHomes({ loggedInUser }) {
   return (
     <>
       <div className="container mt-4">
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "20px",
+          }}
+        >
+         <h4>  <FontAwesomeIcon icon={faHouseCircleCheck} /> Owned Properties</h4>
+        </div>
         <div className="d-flex flex-wrap">
           {homes
             .filter((h) => h.userProfileId === loggedInUser.id)
             .map((home) => (
-              <Card key={`home-${home.id}`} style={{ width: "20rem", margin: '2px' }}>
+              <Card
+                key={`home-${home.id}`}
+                style={{ width: "20rem", margin: "2px" }}
+                className="hover-card"
+              >
                 <Link to={`/homes/${home.id}`}>
-                <CardImg
-                  style={{ objectFit: "cover", height: "200px" }}
-                  variant="top"
-                  src={home.homeImage}
-                  alt="homeimg"
-                />
+                  <div className="card-img-container">
+                    <CardImg
+                      style={{ objectFit: "cover", height: "200px" }}
+                      variant="top"
+                      src={home.homeImage}
+                      alt="homeimg"
+                    />
+                    <div className="card-overlay">
+                      <p>Show Details</p>
+                    </div>
+                  </div>
                 </Link>
                 <CardBody>
-                  <CardText  style={{ marginBottom: "4px"}}>
+                  <CardText style={{ marginBottom: "4px" }}>
                     <strong>
-                    <FontAwesomeIcon icon={faCalendarCheck} /> Purchased on{" "}
+                      <FontAwesomeIcon icon={faCalendarCheck} /> Purchased on{" "}
                       {new Date(home.purchasedOn).toLocaleDateString("en-US")}
                     </strong>{" "}
                     <br />
@@ -43,9 +66,10 @@ export default function UserHomes({ loggedInUser }) {
                     <strong>{home.bathNumber}</strong> ba |{" "}
                     <strong>{home.squareFeet}</strong> sqft
                     <br />
-                    <FontAwesomeIcon icon={faLocationDot} /> {home.streetAddress}, {home.city}, TN
+                    <FontAwesomeIcon icon={faLocationDot} />{" "}
+                    {home.streetAddress}, {home.city}, TN
                   </CardText>
-                 <br/>
+                  <br />
                 </CardBody>
               </Card>
             ))}
